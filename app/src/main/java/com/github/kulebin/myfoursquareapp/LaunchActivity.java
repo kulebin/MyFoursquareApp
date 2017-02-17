f02ca7d6a8590a4b4b5440da4f2ac396afa7e1a4
Mikhail_Sianko <sianko.mg@gmail.com>
2017-02-17 07:37:45 +0300

    Splash screen implemented

--- app/src/main/java/com/github/kulebin/myfoursquareapp/LaunchActivity.java
+++ app/src/main/java/com/github/kulebin/myfoursquareapp/LaunchActivity.java
@@ -1,13 +1,47 @@
 package com.github.kulebin.myfoursquareapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class LaunchActivity extends AppCompatActivity {
 
+    private static final int SPLASH_SHOW_TIME = 1000;
+
+    private Runnable mRunnable;
+    private Handler mHandler;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        mHandler = new Handler();
+        initRunnable();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mHandler.postDelayed(mRunnable, SPLASH_SHOW_TIME);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mHandler.removeCallbacks(mRunnable);
+    }
+
+    private void initRunnable() {
+        mRunnable = new Runnable() {
+
+            @Override
+            public void run() {
+                final Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
     }
 }
---
+++ app/src/main/java/com/github/kulebin/myfoursquareapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.kulebin.myfoursquareapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
