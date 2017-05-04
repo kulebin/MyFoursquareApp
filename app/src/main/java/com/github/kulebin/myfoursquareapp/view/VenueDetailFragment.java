1c424e81dd4c1b57511b0aff37a227a551d5a391
Mikhail_Sianko <sianko.mg@gmail.com>
2017-05-05 01:21:37 +0300

    Side navigation menu implemented

--- app/src/main/java/com/github/kulebin/myfoursquareapp/view/VenueDetailFragment.java
+++ app/src/main/java/com/github/kulebin/myfoursquareapp/view/VenueDetailFragment.java
@@ -2,8 +2,6 @@ package com.github.kulebin.myfoursquareapp.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,7 +18,6 @@ public class VenueDetailFragment extends AbstractBaseFragment implements VenueDe
 
     public static final String TAG = VenueDetailFragment.class.getSimpleName();
     private static final String VENUE_ID = "venueId";
-    private static final String IS_TWO_PANE_MODE = "isTwoPaneMode";
 
     private final VenueDetailContract.Presentation mPresenter = new VenueDetailPresenter(this);
     @BindView(R.id.image_venue)
@@ -34,17 +31,13 @@ public class VenueDetailFragment extends AbstractBaseFragment implements VenueDe
     @BindView(R.id.text_venue_description)
     TextView mVenueDescriptionText;
 
-    public static VenueDetailFragment newInstance(final String pVenueId, final boolean isTabletMode) {
+    public static VenueDetailFragment newInstance(final String pVenueId) {
         final VenueDetailFragment fragment = new VenueDetailFragment();
         final Bundle args = new Bundle();
         args.putString(VENUE_ID, pVenueId);
-        args.putBoolean(IS_TWO_PANE_MODE, isTabletMode);
         fragment.setArguments(args);
-        return fragment;
-    }
 
-    public static VenueDetailFragment newInstance(final String pVenueId) {
-        return newInstance(pVenueId, false);
+        return fragment;
     }
 
     @Override
@@ -52,14 +45,6 @@ public class VenueDetailFragment extends AbstractBaseFragment implements VenueDe
         ButterKnife.bind(this, view);
 
         if (getArguments() != null) {
-            if (!getArguments().getBoolean(IS_TWO_PANE_MODE)) {
-                final ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
-
-                if (actionBar != null) {
-                    actionBar.setDisplayHomeAsUpEnabled(true);
-                }
-            }
-
             mPresenter.onViewCreated(getArguments().getString(VENUE_ID));
         } else {
             Toast.makeText(getContext(), R.string.ERROR_TEXT_NO_VENUE_ID, Toast.LENGTH_LONG).show();
